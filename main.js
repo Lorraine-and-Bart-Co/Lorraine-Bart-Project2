9601e6adef991cc9910f31b174b3b49d6183bffe
Lorraine <lorraine.marie@live.ca>
2022-01-01 16:35:19 -0500

    adding script :star::star::star:

--- main.js
+++ main.js
@@ -9,12 +9,60 @@
 // If a random dog giph is received then we can pass it to a 
 // function that will display it on the page
 
+const dogFinderApp = {};
+
+// Create an init method
+dogFinderApp.init = () => {
+    dogFinderApp.getDoggo();
+    console.log('what is happening');
+};
+
+// Store URL on Dog Finder App a property 
+
+dogFinderApp.url = 'https://api.thedogapi.com/images/search';
+
+// Store API key on the Dog Finder App object as a property:
+
+dogFinderApp.apiKey = '67a37282-d23d-40cd-8461-024b9f0a2266';
+
+// Fetch request to the Dog API 
+
+dogFinderApp.getDoggo = () => {
+    console.log('my name is');
+    const url = new URL(dogFinderApp.url);
+    url.search = new URLSearchParams({
+        client_id: dogFinderApp.apiKey
+    })
+
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((jsonData) => {
+        dogFinderApp.displayDoggo(jsonData);
+        console.log(jsonData);
+    })
+    .catch((err) => {
+        console.log(`Your error is ${err}.`);
+    })
+}
+
+
+
 // we need a "display to page" function that will display tha giphy
 // this function will clear everything on the page first
 // we do this using innerHTML = ''; on the giphy container
+
+dogFinderApp.displayDoggo = (dogObject) => {
+    const img = document.createElement('img');
+}
+
+
 
 // just an idea
 // the giphy container will have an id and when the button/link is clicked <a href="#giphy-container"></a>
 
 // stretch goals
 // Choose your own adventure button that will lead to either a giph dog, a breed funny dog, or a random dog
+
+dogFinderApp.init();
